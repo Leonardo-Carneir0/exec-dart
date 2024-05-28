df4c1fc635705e76865f16f7737a350eb4fbd741
Leonardo Carneiro <69646391+Leonardo-Carneir0@users.noreply.github.com>
2024-05-28 20:09:42 -0300

    Resposta da 4 questão do exercício 3

---
+++ Atividade 3/h4.dart
@@ -0,0 +1,45 @@
+class Livro {
+  String titulo;
+  String autor;
+  int numeroPaginas;
+  bool _estaDisponivel = true;
+
+  Livro(this.titulo, this.autor, this.numeroPaginas);
+
+  void emprestar () {
+    if (_estaDisponivel) {
+      _estaDisponivel = false;
+      print('O livro "$titulo" foi emprestado.');
+    }else{
+      print('O livro "$titulo" não está disponível para empréstimo');
+    }
+  }
+  void devolver(){
+    if (!_estaDisponivel){
+      _estaDisponivel = true;
+      print('O livro" $titulo" "já esta disponível na biblioteca.');
+    }
+  }
+  bool estaDisponivel() {
+    return _estaDisponivel;
+  }
+}
+void main() {
+  Livro livro = Livro('1984', 'George Orwell', 328);
+
+  print('O livro "${livro.titulo}" está disponível? ${livro.estaDisponivel()}');
+
+  livro.emprestar();
+
+  livro.emprestar();
+
+  livro.devolver();
+
+  livro.devolver();
+
+  print('O livro "${livro.titulo}" está disponível? ${livro.estaDisponivel()}');
+}
+
+
+
+
